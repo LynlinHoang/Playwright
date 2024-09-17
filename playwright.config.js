5e458838e8fad79529300c6df9b8f2e97b7cc650
LynlinHoang <hoangthilin93@gmail.com>
2024-09-17 11:32:46 +0700

    img playwright

--- playwright.config.js
+++ playwright.config.js
@@ -43,10 +43,7 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Firefox'] },
     },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+
 
     /* Test against mobile viewports. */
     // {
